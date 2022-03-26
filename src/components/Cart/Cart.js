c3d97f7ac7c151d0c01bbb3b7058d25edec4790b
farabi1 <rashidfarabi@gmail.com>
2022-03-26 22:30:43 +0600

    Bonus Part

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './Cart.css';
 
-const Cart = ({ cart }) => {
-    // const { cart } = props;
+const Cart = (props) => {
+    const { cart } = props;
+    console.log(cart);
     return (
         <div className='cart-container'>
             <h1>Cart Part </h1>
 
-            <h1>Selected Item : {cart.length}</h1>
+            <h3>Selected Item : {cart.length}</h3>
+            <p>Selected Item :
+                {
+                    cart.map((item) => (
+                        <h1>{item.name}</h1>
+                    ))
+                }
+            </p>
             <button className='choose-button'><p className='choose-button-text' >Choose 1 For Me</p>
             </button>
             <button className='choose-button'><p className='choose-button-text' >Reset Cart</p>
