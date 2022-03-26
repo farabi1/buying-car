c5ae6102325ed88d4f71ff01bfc4951488695cf6
farabi1 <rashidfarabi@gmail.com>
2022-03-26 12:35:43 +0600

    Data Show Setup

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+
+
     return (
         <div className='shop-container'>
             <div className="product-container">
+                <h2>Products : {products.length}</h2>
                 <Product></Product>
             </div>
             <div className="cart-container">
